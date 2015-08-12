5ac81bc5161ff046753d63639e5135d701096dc8
Jason Lea-Jones <jleajones@gmail.com>
2015-08-12 09:34:36 -0400

    made some minor tweaks to polish

--- app/scripts/directives/jljmoviebase.js
+++ app/scripts/directives/jljmoviebase.js
@@ -10,18 +10,23 @@
     function MovieBase($animate) {
 
         return {
-            template: '<figure class="card large" ng-mouseenter="expand($event)" ng-mouseleave="collapse($event)">' +
+            template: '<figure class="card large" ng-mouseenter="expand($event)" ng-mouseleave="collapse($event)"><div class="row">' +
                 '<a ng-href="#/movies/{{ movie.title | jljEncodeUri }}"><img ng-src="{{ movie.posters.original | jljRottenTomatoImage }}" width="130" />' +
                 '</a>' +
 
                 '<figcaption>' +
-                '<div class="col-xs-12"><div ng-if="movie.release_dates.theater">' +
-                '<p class="uppercase"><b><i>{{ released ? "released" : "opens" }} {{ movie.release_dates.theater | date:\'EEEE\' }}</i></b>' +
-                '&nbsp;&nbsp;{{ movie.release_dates.theater | date:\'MMMM d, yyyy\' }}</p>' +
-                '</div>' +
+                '<header class="col-xs-12 "><div ng-if="movie.release_dates.theater"><h3>' +
+
+                '<small ng-switch="released">' +
+                '<s ng-switch-when="true"><b class="uppercase"><i> opens {{ movie.release_dates.theater | date:\'EEEE\' }}</i></b>' +
+                '&nbsp;&nbsp;{{ movie.release_dates.theater | date:\'MMMM d, yyyy\' }}</s>' +
+
+                '<span ng-switch-when="false"><b class="uppercase"><i> opens {{ movie.release_dates.theater | date:\'EEEE\' }}</i></b>' +
+                '&nbsp;&nbsp;{{ movie.release_dates.theater | date:\'MMMM d, yyyy\' }}</span>' +
+                '</small></h3></div>' +
 
                 '<h3><a ng-href="#/movies/{{ movie.title | jljEncodeUri }}">{{ movie.title }}</a> <a href=""><span class="glyphicon glyphicon-heart-empty"></span></a></h3>' +
-                '<p><i>{{ movie.mpaa_rating }}, {{ movie.runtime | jljHumanTime:\'mm\':\'hhh mmm\':false }}</i></p></div>' +
+                '<p><i>{{ movie.mpaa_rating }}, {{ movie.runtime | jljHumanTime:\'mm\':\'hhh mmm\':false }}</i></p></header>' +
 
                 '<div class="col-md-6 col-xs-12"><p><b class="capitalize">audience rating: </b>' +
                 '<jlj-star-rating rating="{{ movie.ratings.audience_score }}"></jlj-star-rating></p></div>' +
@@ -30,10 +35,10 @@
                 '</p></div>' +
                 '<div class="col-xs-12"><p>{{ movie.synopsis | limitTo: 150}}...</p>' +
                 '<p><a ng-href="#/movies/{{ movie.title | jljEncodeUri }}">Read More</a></p></div>' +
-                '</figcaption>' +
-                '<div class="content"><div class="row"><div class="col-xs-12 col-md-3">' +
-                '<h5 class="capitalize"><span class="glyphicon glyphicon-user"></span> cast</h5><ul class="list-unstyled"><li ng-repeat="member in movie.abridged_cast"><a href="">{{ member.name }}</a> <small>playing <b ng-repeat="part in member.characters">{{ part }}</span></b></small></li>' +
-                '</div><div class="col-xs-12 col-md-6">' +
+                '</figcaption></div>' +
+                '<div class="content"><div class="row"><div class="col-xs-12 col-md-4">' +
+                '<h5 class="capitalize"><span class="glyphicon glyphicon-user"></span> cast</h5><ul class="list-unstyled"><li ng-repeat="member in movie.abridged_cast"><a href="">{{ member.name }}</a> <small ng-if="member.characters.length > 0">playing <b ng-repeat="part in member.characters">{{ part }}</span></b></small></li>' +
+                '</div><div class="col-xs-12 col-md-8">' +
                 '<h5 class="capitalize"><span class="glyphicon glyphicon-film"></span> clips</h5></div></div>' +
                 '<div class="row"><div class="col-xs-6"><button type="button" class="btn btn-success btn-lg btn-block capitalize"><span class="glyphicon glyphicon-thumbs-up"></span> good</button></div>' +
                 '<div class="col-xs-6"><button type="button" class="btn btn-danger btn-lg btn-block capitalize"><span class="glyphicon glyphicon-thumbs-down"></span> bad</button></div></div>' +

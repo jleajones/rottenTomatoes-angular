8ebda3079d0071a840e3c4a2bacbc6c6d0dc4eb6
Jason Lea-Jones <jleajones@gmail.com>
2015-08-12 08:58:24 -0400

    added buttons to card

--- app/scripts/directives/jljmoviebase.js
+++ app/scripts/directives/jljmoviebase.js
@@ -11,7 +11,7 @@
 
         return {
             template: '<figure class="card large" ng-mouseenter="expand($event)" ng-mouseleave="collapse($event)">' +
-                '<a ng-href="#/movies/{{ movie.id | jljEncodeUri }}"><img ng-src="{{ movie.posters.original | jljRottenTomatoImage }}" width="130" />' +
+                '<a ng-href="#/movies/{{ movie.title | jljEncodeUri }}"><img ng-src="{{ movie.posters.original | jljRottenTomatoImage }}" width="130" />' +
                 '</a>' +
 
                 '<figcaption>' +
@@ -20,7 +20,7 @@
                 '&nbsp;&nbsp;{{ movie.release_dates.theater | date:\'MMMM d, yyyy\' }}</p>' +
                 '</div>' +
 
-                '<h3><a href="">{{ movie.title }}</a> <a href=""><span class="glyphicon glyphicon-heart-empty"></span></a></h3>' +
+                '<h3><a ng-href="#/movies/{{ movie.title | jljEncodeUri }}">{{ movie.title }}</a> <a href=""><span class="glyphicon glyphicon-heart-empty"></span></a></h3>' +
                 '<p><i>{{ movie.mpaa_rating }}, {{ movie.runtime | jljHumanTime:\'mm\':\'hhh mmm\':false }}</i></p></div>' +
 
                 '<div class="col-md-6 col-xs-12"><p><b class="capitalize">audience rating: </b>' +
@@ -29,12 +29,15 @@
                 '<jlj-star-rating rating="{{ movie.ratings.critics_score }}">' +
                 '</p></div>' +
                 '<div class="col-xs-12"><p>{{ movie.synopsis | limitTo: 150}}...</p>' +
-                '<p><a href="">Read More</a></p></div>' +
+                '<p><a ng-href="#/movies/{{ movie.title | jljEncodeUri }}">Read More</a></p></div>' +
                 '</figcaption>' +
-                '<div class="content row"><div class="col-xs-12 col-md-3">' +
-                '<h5 class="capitalize">cast</h5><ul class="list-unstyled"><li ng-repeat="member in movie.abridged_cast"><a href="">{{ member.name }}</a> <small>playing <b ng-repeat="part in member.characters">{{ part }}</span></b></small></li>' +
+                '<div class="content"><div class="row"><div class="col-xs-12 col-md-3">' +
+                '<h5 class="capitalize"><span class="glyphicon glyphicon-user"></span> cast</h5><ul class="list-unstyled"><li ng-repeat="member in movie.abridged_cast"><a href="">{{ member.name }}</a> <small>playing <b ng-repeat="part in member.characters">{{ part }}</span></b></small></li>' +
                 '</div><div class="col-xs-12 col-md-6">' +
-                '<h5 class="capitalize">clips</h5></div></div>' +
+                '<h5 class="capitalize"><span class="glyphicon glyphicon-film"></span> clips</h5></div></div>' +
+                '<div class="row"><div class="col-xs-6"><button type="button" class="btn btn-success btn-lg btn-block capitalize"><span class="glyphicon glyphicon-thumbs-up"></span> good</button></div>' +
+                '<div class="col-xs-6"><button type="button" class="btn btn-danger btn-lg btn-block capitalize"><span class="glyphicon glyphicon-thumbs-down"></span> bad</button></div></div>' +
+                '</div></div>' +
                 '</figure>',
             restrict: 'E',
             scope: {
